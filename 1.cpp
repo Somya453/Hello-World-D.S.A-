f01bddc6a97ae23a6c548c2def372414643dd9a4
somya <somyasharma0488@gmail.com>
2026-01-21 18:19:08 +0530

    First

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter the number of times to print 'Prince': ";
+    cin>>n;
+
+    for(int i=1; i<=n; i++){
+        cout<<"Prince"<<endl;
+    }
+    return 0;
+}
