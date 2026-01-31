31032e94ef775cc638a3c58f111f22cde0c4915f
somya <somyasharma0488@gmail.com>
2026-01-31 21:09:29 +0530

    main

---
+++ 8.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int a, b, c, d;
+	    cin>>a>>b>>c>>d;
+	    
+	    if((a==c && b==d) || (a==b && c==d) || (a==d && c==b))
+	        cout<<"YES"<<endl;
+	        
+	        
+	   else
+	        cout<<"NO"<<endl;
+	    
+	    
+	}
+
+}
